74e9926ef4af33a26383ea9ceb73f33e0ab89ba8
Nischay <gladiatoruk1234@gmail.com>
2025-02-12 16:29:46 +0530

    Updated Files

--- src/App.js
+++ src/App.js
@@ -8,30 +8,40 @@ import Alert from "./components/Alert";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
 import { useState } from "react";
+import Background from "./components/Background";
 
 function App() {
   const [alert, setAlert] = useState(null);
-  const showAlert = (message, type)=>{
+  const showAlert = (message, type) => {
     setAlert({
       msg: message,
-      type: type
-    })
-    setTimeout(()=>{
+      type: type,
+    });
+    setTimeout(() => {
       setAlert(null);
     }, 1500);
-  }
+  };
   return (
     <>
       <NoteState>
         <BrowserRouter>
           <Navbar />
-          <Alert alert={alert}/>
+          <Background />
+          <Alert alert={alert} />
           <div className="container">
             <Routes>
-              <Route exact path="/" element={<Home showAlert={showAlert}/>} />
+              <Route exact path="/" element={<Home showAlert={showAlert} />} />
               <Route exact path="/about" element={<About />} />
-              <Route exact path="/login" element={<Login showAlert={showAlert}/>} />
-              <Route exact path="/signup" element={<Signup showAlert={showAlert}/>} />
+              <Route
+                exact
+                path="/login"
+                element={<Login showAlert={showAlert} />}
+              />
+              <Route
+                exact
+                path="/signup"
+                element={<Signup showAlert={showAlert} />}
+              />
             </Routes>
           </div>
         </BrowserRouter>
---
+++ src/components/Background.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Background.css"; // Import the CSS file
+
+const Background = () => {
+  return <div className="background-image"></div>;
+};
+
+export default Background;
