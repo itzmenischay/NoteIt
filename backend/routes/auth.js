3cafa1eb4a7c30d15cb2db884637c2be835534ad
Nischay <gladiatoruk1234@gmail.com>
2025-02-12 15:52:58 +0530

    Updated Files

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -6,7 +6,7 @@ const bcrypt = require("bcryptjs");
 var jwt = require("jsonwebtoken");
 var fetchuser = require("../middleware/fetchuser");
 
-const JWT_SECRET = "Harryisagoodb$oy";
+const JWT_SECRET = "Nischay@";
 
 // ROUTE 1: Create a User using: POST "/api/auth/createuser". No Login required
 router.post(
